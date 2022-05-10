d34387c51709f7bc000da051008f57020ffdb5ed
Sara Barros <65254818+sarashb@users.noreply.github.com>
2022-05-10 11:13:02 -0300

    lista de jogos

--- jogoteca.py
+++ jogoteca.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 
 @app.route('/inicio')
 def ola():
-    return render_template('lista.html')
+    lista = ['Tetris', 'Skyrim', 'Crash']
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 app.run()
